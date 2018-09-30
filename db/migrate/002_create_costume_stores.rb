92cdf8c75bf52f99c2680b1728dc1d5777e9d9eb
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-30 12:39:00 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,7 +3,7 @@
 class CostumeStores < ActiveRecord::Migation[4.2]
 
   def change
-      create_table :costumestores do |t|
+      create_table :costume_stores do |t|
           t.string :name
           t.string :location
           t.integer :costume_inventory
