d592d505ec3b6d21cf26f44bc1243092ca7e976f
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-30 12:46:40 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -4,7 +4,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change
     create_table :haunted_houses do |t|
       t.string :name
-      t.string :address
+      t.string :location
       t.string :theme
       t.float :price
       t.boolean :family_friendly
