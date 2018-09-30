06d1e88d06e4e21aa9a53c9ae4599eb8dfcb1b32
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-30 12:37:31 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,17 @@
 # Create your costume_stores migration here
+
+class CostumeStores < ActiveRecord::Migation[4.2]
+
+  def change
+      create_table :costumestores do |t|
+          t.string :name
+          t.string :location
+          t.integer :costume_inventory
+          t.integer :num_of_employees
+          t.boolean :still_in_business
+          t.string :opening_time
+          t.string :closing_time
+      end
+  end
+  
+end
