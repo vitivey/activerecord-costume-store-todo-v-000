a6cb7be6e1d797ee2b970ff1a49d9a116a1a54ee
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-30 12:26:12 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -7,12 +7,10 @@
 # Define a change method in which to do the migration
 # In this change method, create columns with the correct names and
 # value types according to the spec
-require "pry"
 class CreateCostumes< ActiveRecord::Migration
 
   def change
     create_table :costume do |t|
-      binding.pry
       t.string :name
       t.float :price
       t.string :url
