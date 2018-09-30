80555fe8f3575b2173235b1c8e8ea0e23fca51c8
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-30 12:33:05 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,7 +13,7 @@ class CreateCostumes< ActiveRecord::Migration[4.2]
     create_table :costumes do |t|
       t.string :name
       t.float :price
-      t.string :url
+      t.string :image_url
       t.string :size
       t.timestamps
     end
